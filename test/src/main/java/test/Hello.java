c04f1cd8f6257ffc038f5b46fe51d2c2aa146f4c
Administrator <Administrator@ZLMU2AJR3QSU17R>
2018-05-23 19:15:31 +0800

    第一个版本

---
+++ test/src/main/java/test/Hello.java
@@ -0,0 +1,29 @@
+/**  
+* @Title: Hello.java  
+* @Package test  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author Administrator  
+* @date 2018年5月23日  
+* @version V1.0  
+*/ 
+package test;
+
+/**  
+* <p>Title: Hello</p>  
+* <p>Description: </p>  
+* @author shenlan  
+* @date 2018年5月23日  
+*/
+public class Hello {
+
+	/**  
+	 * <p>Title: main</p>  
+	 * <p>Description: </p>  
+	 * @param args  
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
